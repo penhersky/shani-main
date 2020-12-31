fb3f6dd1452926fe3ed21df0382c232c21f832d0
penhersky <penxerscky@gmail.com>
2020-12-31 11:34:27 +0200

    add auth io middleware

--- src/io/middleware.ts
+++ src/io/middleware.ts
@@ -1,6 +1,17 @@
-import { Socket } from 'socket.io';
+import jwt from 'jsonwebtoken';
 
-export default (socket: Socket, next: any) => {
-  console.log('socket', socket);
-  next();
+import { add } from './users';
+import { ACCESS_SERVICE_SECURITY_TOKEN_KEY } from '../config';
+
+export default (socket: any, next: any) => {
+  try {
+    const result = <string | any>(
+      jwt.verify(socket?.auth?.token, String(ACCESS_SERVICE_SECURITY_TOKEN_KEY))
+    );
+    if (typeof result === 'string') socket.disconnect();
+    add(result.userId, socket.id);
+    next();
+  } catch (error) {
+    socket.disconnect();
+  }
 };
