da8d054c55d857fe6e18a99d561cc7eae984bd23
penhersky <penxerscky@gmail.com>
2021-01-11 12:32:36 +0200

    change order type resolv

--- src/resolvers/Order/Order.ts
+++ src/resolvers/Order/Order.ts
@@ -1,15 +1,30 @@
 import { Request, Comment } from '../../models';
 import cather from '../../wrappers/typeCather';
+import authUser from '../../lib/checkAuth';
 
 import categories from '../Category/Resolve';
 import user from '../User/resolve';
 
 export default {
   categories,
-  requests: async (root: any) =>
-    cather(async () => Request.find({ order: root.id })),
-  comments: async (root: any) =>
-    cather(async () => Comment.find({ order: root.id })),
+  requests: async (root: any, args: any, context: any) =>
+    cather(
+      async (auth: any) => {
+        if (auth.id === root.customer) return Request.find({ order: root.id });
+        return Request.find({ order: root.id, visible: true });
+      },
+      context,
+      authUser,
+    ),
+  comments: async (root: any, args: any, context: any) =>
+    cather(
+      async () => async (auth: any) => {
+        if (auth.id === root.customer) return Comment.find({ order: root.id });
+        return Comment.find({ order: root.id, visible: true });
+      },
+      context,
+      authUser,
+    ),
   customer: user('customer'),
   performer: user('performer'),
 };
