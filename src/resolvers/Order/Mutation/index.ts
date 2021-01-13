b97852dce77fe18f700f13f2df1d755d9c2b5c77
penhersky <penxerscky@gmail.com>
2021-01-13 11:15:01 +0200

    add setOrderVisible

--- src/resolvers/Order/Mutation/index.ts
+++ src/resolvers/Order/Mutation/index.ts
@@ -1,2 +1,3 @@
 export { default as createOrder } from './createOrder';
 export { default as setOrderCommented } from './setOrderCommented';
+export { default as setOrderVisible } from './setOrderVisible';
--- src/typeDefs/Mutation.ts
+++ src/typeDefs/Mutation.ts
@@ -4,13 +4,13 @@ export default gql`
   type Mutation {
     createOrder(order: OrderInput!): OrderResult!
     updateOrder(id: ID!, order: OrderInput!): Result!
-    setOrderVisible(id: ID!): Result! # -
+    setOrderVisible(id: ID!): Result!
     # order status
     setOrderStatusToDone(id: ID!): Result! # -
     setOrderStatusToClosed(id: ID!): Result! # -
     setOrderStatusToCanceled(id: ID!): Result! # -
     # performer
-    setOrderPerformer(id: ID!): Result! # -
+    setOrderPerformer(id: ID!, performer: ID!): Result! # -
     cancelOrderPerformer(id: ID!): Result! # -
     deleteOrder(id: ID!): Result! # -
     #
